46ccad8eb4d5903796d7ad0d8deb140fe4b6872a
raajiappu <74717403+raajiappu@users.noreply.github.com>
2020-12-15 16:31:22 +0100

    Add files via upload

---
+++ essentialScript.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function essentials() {
+    document.getElementById("header").innerHTML = "<span id=" + " title" + ">e-Voting</span><a id='hlogin' href='#'> Login</a>";
+
+    document.getElementById("footer").innerHTML =
+        " <p><b>  " + "Our lives begin to end the day we become silent about things that matter." + "</b > <br />"
+    + "&#8212;  Dr.Martin Luther King, Jr., civil rights leader and Nobel Peace Prize laureate </p>";
+}
